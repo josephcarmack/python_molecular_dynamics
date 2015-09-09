d547c96d1df46d35e68e4d7f0283283b4fe23b42
joseph <joseph.liping@gmail.com>
2015-09-09 17:46:47 -0500

    added function for forces + other stuff
    - added function to calculated forces to 'functions.py'
    - added function to get pbc to 'functions.py'
    - continued developing main.py

--- functions.py
+++ functions.py
@@ -38,16 +38,53 @@ def pos_init(pos,dim,L):
 
     return pos;
 
-def calc_repulsion_force(rij,FR):
+def calc_repulsion_force(rij,rad,sigma):
     """
-    This function takes the separation distance between two particles, rij and
-    the repulsive force constant, FR, and calculates the repulsion force between
-    the two. It returns this force value.
+    This function takes the separation distance between two particles, rij, the
+    particle radius, rad, and the repulsive force constant, sigma, and calcu-
+    lates the repulsion force between the two. It returns this force value.
 
     if we are calculating the force on particle i then rij = |ri - rj| and the
     force will point in the same direction as the vector ri - rj.
     """
 
-    rep_force = FR/rij**7
+    rep_force = sigma/(rij-2*rad)**7
 
     return rep_force;
+
+def calc_rij_pbc(ri,rj,dim,box):
+    """
+    This function takes the vector positions of two particles, i and j, and cal-
+    culates their separation distance assuming periodic boundary conditions
+    (pbc). It also calculates the unit vector of the of the rij_pbc vector,
+    'rij_unit'.
+
+    INPUTS:
+    ri = position of particle i
+    rj = position of particle j
+    dim = the dimension of the simulation (i.e. 1-,2-, or 3-dimensions)
+    box = array with 'dim' components. each component is the length of the box
+            in the respective dimension.
+
+    OUTPUTS:
+    rij_mag = the magnitude of the separation vector with pbc.
+    rij_unit = separtion vector with pbc made into a unit vector.
+
+    """
+    import numpy as n
+
+    # implement periodic boundary conditions
+    rij_pbc = n.zeros(dim)
+    rij_mag = 0.0
+    for d in range(dim):
+        rij_pbc[d] = ri[d] - rj[d]
+        if abs(rij_pbc[d]) > box[d]/2:
+            rij_pbc[d] = rij_pbc[d] + box[d]
+        rij_mag = rij_mag + rij_pbc[d]**2
+
+    rij_mag = n.sqrt(rij_mag)
+
+    # calculate unit vector with pbc
+    rij_unit = 1/rij_mag*rij_pbc
+
+    return rij_mag,rij_unit;
--- main.py
+++ main.py
@@ -12,28 +12,70 @@ vel = zeros((N,dim))                # array for storing particle velocities
 force = zeros((N,dim))              # array to store particle forces
 efield = zeros(dim)                 # array to store efield vector
 eMag = 10.0                         # magnitude of the efield
-rad = 5.0                           # particle radius
+rad = 1.0                           # particle radius (microns)
+density = 5.0                       # particle density (micro-grams/micron^3)
+vol = 4.0/3.0*pi*rad**3             # particle volume (microns^3)
+mass = density*vol                  # particle mass (micro-grams)
+vel_spread = 2.0                    # std 4 velocity generator (micron/microsec)
 
 # integration scheme parameters
-dt = 0.01                           # time step
+dt = 0.01                           # time step (micro-seconds)
 steps = 1000                        # number of steps to integrate
 total_time = dt*steps               # total simulation time
 
 # particle interaction parameters
-FR = 1.0                            # repulsive force constant
+sigma = 1.0                         # the leonard-jones parameter particle-part-
+                                    # icle interactions.
 
 # simulation domain
-L = 100.0                           # box length
-box = zeros((dim))                  # initialize box
-for dimension in box:
-    dimension = L                   # set box dimensions
+L = 100.0                           # box length (microns)
+box = zeros(dim)                    # initialize box
+for i in range(len(box)):
+    box[i] = L                      # set box dimensions
 
 # initialize system assuming electric field is on and particles instantly polar-
 # ize and their dipoles rotate to align with the electric field direction
 
 efield[0] = eMag                    # set direction and magnitude of efield
-pos = f.pos_init(pos,dim,L)         # initialize positions
-vel = f.vel_init(vel,dim,2.0)       # initialize velocities
+# pos = f.pos_init(pos,dim,L)       # initialize positions
+pos[0] = array([50.0,45.0])
+pos[1] = array([50.0,60.0])
+vel = f.vel_init(vel,dim,vel_spread)# initialize velocities
 
+# EVOLVE SYSTEM
+
+# for step in range(steps):
+
+    # zero out forces
+
+for i in range(N):
+    for j in range(i+1,N):
+
+        # get positions of particle i and j
+        ri = pos[i]
+        rj = pos[j]
+
+        # get separation distance and unit vector that points in the direct-
+        # ion of the force on particle i for the i-j particle pair.
+        rij_mag,rij_unit = f.calc_rij_pbc(ri,rj,dim,box)
+
+        # get force on particle i from particle j
+        fmag = f.calc_repulsion_force(rij_mag,rad,sigma)
+        force[i] = force[i] + fmag*rij_unit
+
+        # applying Newton's third law to get force on particle j from part-
+        # icle j
+        force[j] = force[j] - fmag*rij_unit
+
+# update the velocities and then positions based on the calculated forces
+for i in range(N):
+    # get the net acceleration on particle i from its net force
+    ai = 1/mass*force[i]
+    # update the velocity of particle i using its acceleration
+    vel[i] = vel[i] + dt*ai
+    # update the position of particle i using its updated velocity
+    pos[i] = pos[i] + dt*vel[i]
+
+print pos
 
 print "this is the end of the script!"
