f42f8089abead51f4ebe2b8ee7fad253af21b9fa
joseph <joseph.liping@gmail.com>
2015-09-10 16:54:16 -0500

    finished main.py minus dipole interactions
    - created several new functions in functions.py
    - still need to add the dipole interactions
    - also the repulsive particle interactions still need
      need work. the system blows up sometimes due to
      overlapping particles.

--- functions.py
+++ functions.py
@@ -1,27 +1,9 @@
-def vel_init(vel,dim,sigma):
-    """
-    This function populates the velocity array with random velocities from
-    a Gaussian distribution.
-    """
-    from random import seed, gauss
-    import numpy as np
-
-    ran = np.zeros(dim)              # array to store random numbers
-    seed()
-
-    for i in range(len(vel)):
-
-        for j in range(dim):
-            ran[j] = gauss(0.0,sigma)
-
-        vel[i] = ran
-
-    return vel;
-
-def pos_init(pos,dim,L):
+def pos_init(pos,dim,box,rad):
     """
     This function populates the position array with random positions from
-    uniformly distributed throughout the box.
+    uniformly distributed throughout the box. It also does not allow part-
+    cles to be placed too close to each other (thus avoiding particles ov-
+    erlapping).
     """
     from random import seed, uniform
     import numpy as np
@@ -31,26 +13,24 @@ def pos_init(pos,dim,L):
 
     for i in range(len(pos)):
 
-        for j in range(dim):
-            ran[j] = uniform(0.0,L)
+        tooClose = True
 
-        pos[i] = ran
+        # check to make sure position is not too close to other particles
+        while tooClose:
+            tooClose = False
+            # get a random position
+            for j in range(dim):
+                ran[j] = uniform(0.0,box[j])
+            # assign position
+            pos[i] = ran
+            # check to see if random position is too close to other positions
+            for k in range(i-1):
+                ri = pos[k]
+                rmag,runit = calc_rij_pbc(ri,ran,dim,box)
+                if rmag < 3.0*rad:
+                    tooClose = True
 
     return pos;
-
-def calc_repulsion_force(rij,rad,sigma):
-    """
-    This function takes the separation distance between two particles, rij, the
-    particle radius, rad, and the repulsive force constant, sigma, and calcu-
-    lates the repulsion force between the two. It returns this force value.
-
-    if we are calculating the force on particle i then rij = |ri - rj| and the
-    force will point in the same direction as the vector ri - rj.
-    """
-
-    rep_force = sigma/(rij-2*rad)**7
-
-    return rep_force;
 
 def calc_rij_pbc(ri,rj,dim,box):
     """
@@ -88,3 +68,125 @@ def calc_rij_pbc(ri,rj,dim,box):
     rij_unit = 1/rij_mag*rij_pbc
 
     return rij_mag,rij_unit;
+
+def vel_init(vel,dim,sigma):
+    """
+    This function populates the velocity array with random velocities from
+    a Gaussian distribution.
+    """
+    from random import seed, gauss
+    import numpy as np
+
+    ran = np.zeros(dim)              # array to store random numbers
+    seed()
+
+    for i in range(len(vel)):
+
+        for j in range(dim):
+            ran[j] = gauss(0.0,sigma)
+
+        vel[i] = ran
+
+    return vel;
+
+def calc_repulsion_force(rij,rad,sigma):
+    """
+    This function takes the separation distance between two particles, rij, the
+    particle radius, rad, and the repulsive force constant, sigma, and calcu-
+    lates the repulsion force between the two. It returns this force value.
+
+    if we are calculating the force on particle i then rij = |ri - rj| and the
+    force will point in the same direction as the vector ri - rj.
+    """
+
+    rep_force = sigma/(rij-2.1*rad)**7
+
+    return rep_force;
+
+def write_point_data_vtk(description,numpy_array,N,dim,step):
+    """
+    This function takes a numpy array that stores 1-, 2-, or 3-dimensional point
+    data as floats, and writes it to a vtkfile with the 'step' as part of the
+    filename. The dimension is indicated by the input variable 'dim'.
+
+    INPUTS:
+
+    description = a string that will be the first part of the vtk filename.
+    numpy_array = numpy array with shape = (N,dim).
+    N = a scalar equal to the number of points in the numpy_array
+    dim = scalar that is equal to 1, 2, or 3.
+    step = a scalar that represents a step from a loop iteration variable.
+
+    OUTPUTS:
+
+    filename_step.vtk = a vtk file with the corresponding point data
+
+    """
+    from numpy import c_,zeros,array
+    import os
+
+    # if dim < 3, pad numpy_array with zeros making it 3-dimensional
+    if dim == 1:
+        numpy_array = c_[numpy_array,zeros((N,2))] # add 2 col of zeros
+    if dim == 2:
+        numpy_array = c_[numpy_array,zeros(N)] # add 1 col of zeros
+
+    # construct the full filename
+    file_name = description + '_' + str(step) + '.vtk'
+
+    # open the file for writing
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    dest_dir = os.path.join(script_dir, 'vtkoutput')
+    try:
+        os.makedirs(dest_dir)
+    except OSError:
+        pass # already exists
+    path = os.path.join(dest_dir, file_name)
+    with open(path, 'wb') as f:
+
+        # write vtk header
+        f.write('# vtk DataFile Version 3.1\n')
+        f.write('VTK file containing ' + description + ' data\n' )
+        f.write('ASCII\n')
+        f.write('DATASET POLYDATA\n')
+        f.write('\n')
+        f.write('POINTS\t' + str(N) + '\tfloat\n')
+
+        # write the point data
+        for i in range(N):
+            f.write('{:>20}'.format(str(numpy_array[i,0]))),
+            f.write('{:>20}'.format(str(numpy_array[i,1]))),
+            f.write('{:>20}'.format(str(numpy_array[i,2])))
+            f.write('\n')
+
+    # close file
+    f.close()
+
+    return 0;
+
+def enforce_pbc(pos,dim,box):
+    """
+    This function takes a particles position and enforces periodic boundary
+    conditions according to the 'box' parameter.
+
+    INPUTS:
+
+    pos = numpy array of shape (dim)
+    dim = scalar value equal to 1, 2, or 3 representing the dimensionality of
+            the simulation (i.e. if dim = 2, pos has 2 components)
+    box = numpy array with of shape (dim) that contains the simulation domain
+            size in the respective dimensions (i.e. if dim = 2, then
+            box = [Lx,Ly])
+
+    OUTPUTS:
+
+    pos = numpy array of shape (dim) altered to comply with periodic boundary
+            conditions.
+    """
+    for d in range(dim):
+        if pos[d] > box[d]:
+            pos[d] = pos[d] - box[d]
+        if pos[d] < 0.0:
+            pos[d] = pos[d] + box[d]
+
+    return pos;
--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 
 from numpy import *
 import functions as f
+import os
+
+# delete old vtk files from previous runs
+os.system('rm -r vtkoutput')
 
 # parameters
 dim = 2                             # number of spacial dimensions
-N = 2                               # number of colloidal particles
+N = 100                             # number of colloidal particles
 pos = zeros((N,dim))                # array for storing particle positions
 vel = zeros((N,dim))                # array for storing particle velocities
 force = zeros((N,dim))              # array to store particle forces
@@ -19,9 +23,13 @@ mass = density*vol                  # particle mass (micro-grams)
 vel_spread = 2.0                    # std 4 velocity generator (micron/microsec)
 
 # integration scheme parameters
-dt = 0.01                           # time step (micro-seconds)
-steps = 1000                        # number of steps to integrate
+dt = 0.001                           # time step (micro-seconds)
+steps = 2500                        # number of steps to integrate
 total_time = dt*steps               # total simulation time
+
+# file io parameters
+skip = 10                           # how many steps to skip between outputs
+description = 'particle'            # a description of the type of vtk data
 
 # particle interaction parameters
 sigma = 1.0                         # the leonard-jones parameter particle-part-
@@ -37,45 +45,55 @@ for i in range(len(box)):
 # ize and their dipoles rotate to align with the electric field direction
 
 efield[0] = eMag                    # set direction and magnitude of efield
-# pos = f.pos_init(pos,dim,L)       # initialize positions
-pos[0] = array([50.0,45.0])
-pos[1] = array([50.0,60.0])
+pos = f.pos_init(pos,dim,box,rad)   # initialize positions
+# pos[0] = array([50.0,45.0])
+# pos[1] = array([50.0,60.0])
 vel = f.vel_init(vel,dim,vel_spread)# initialize velocities
+
+# write initial particle positions to vtk file
+f.write_point_data_vtk(description,pos,N,dim,1)
 
 # EVOLVE SYSTEM
 
-# for step in range(steps):
+for step in range(steps):
 
     # zero out forces
+    force = force*0.0
 
-for i in range(N):
-    for j in range(i+1,N):
+    for i in range(N):
+        for j in range(i+1,N):
 
-        # get positions of particle i and j
-        ri = pos[i]
-        rj = pos[j]
+            # get positions of particle i and j
+            ri = pos[i]
+            rj = pos[j]
 
-        # get separation distance and unit vector that points in the direct-
-        # ion of the force on particle i for the i-j particle pair.
-        rij_mag,rij_unit = f.calc_rij_pbc(ri,rj,dim,box)
+            # get separation distance and unit vector that points in the direct-
+            # ion of the force on particle i for the i-j particle pair.
+            rij_mag,rij_unit = f.calc_rij_pbc(ri,rj,dim,box)
 
-        # get force on particle i from particle j
-        fmag = f.calc_repulsion_force(rij_mag,rad,sigma)
-        force[i] = force[i] + fmag*rij_unit
+            # get force on particle i from particle j
+            fmag = f.calc_repulsion_force(rij_mag,rad,sigma)
+            force[i] = force[i] + fmag*rij_unit
 
-        # applying Newton's third law to get force on particle j from part-
-        # icle j
-        force[j] = force[j] - fmag*rij_unit
+            # applying Newton's third law to get force on particle j from part-
+            # icle j
+            force[j] = force[j] - fmag*rij_unit
 
-# update the velocities and then positions based on the calculated forces
-for i in range(N):
-    # get the net acceleration on particle i from its net force
-    ai = 1/mass*force[i]
-    # update the velocity of particle i using its acceleration
-    vel[i] = vel[i] + dt*ai
-    # update the position of particle i using its updated velocity
-    pos[i] = pos[i] + dt*vel[i]
+    # update the velocities and then positions based on the calculated forces
+    for i in range(N):
+        # get the net acceleration on particle i from its net force
+        ai = 1/mass*force[i]
+        # update the velocity of particle i using its acceleration
+        vel[i] = vel[i] + dt*ai
+        # update the position of particle i using its updated velocity
+        pos[i] = pos[i] + dt*vel[i]
+        # implement periodic boundary conditions
+        f.enforce_pbc(pos[i],dim,box)
 
-print pos
+    # write output ever skip steps
+    if step > 0:
+        if (step % skip) == 0:
+            f.write_point_data_vtk(description,pos,N,dim,step)
 
-print "this is the end of the script!"
+# write final step output
+f.write_point_data_vtk(description,pos,N,dim,steps)
